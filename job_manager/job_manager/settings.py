436a4b242522775721a6778a92ff76a044fbbe3c
Jonathan Duperrier <jonathan.duperrier@gmail.com>
2017-11-08 11:45:49 +0100

    karma integration User factory

--- job_manager/job_manager/settings.py
+++ job_manager/job_manager/settings.py
@@ -152,6 +152,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "lib"),
     os.path.join(BASE_DIR, "nmpi-job-manager-app"),
     os.path.join(BASE_DIR, "nmpi-dashboard-app"),
+    os.path.join(BASE_DIR, "job_manager/nmpi-job-manager-app"),
+    os.path.join(BASE_DIR, "job_manager/nmpi-dashboard-app"),
+    os.path.join(BASE_DIR, "job_manager/static"),
 ]
 STATIC_ROOT = "%s/static/" % BASE_DIR
 
---
+++ job_manager/nmpi-job-manager-app/nmpi/user/model.spec.js
@@ -0,0 +1,28 @@
+// Be descriptive with titles here. The describe and it titles combined read like a sentence.
+describe('User factory', function() {
+  // it('has a dummy spec to test 2 + 2', function() {
+  //   // An intentionally failing test. No code within expect() will never equal 4.
+  //   expect(2 + 2).toEqual(4);
+  // });
+
+  var User;
+  //var nmpi;
+  
+  // Before each test load our api.users module
+  beforeEach(angular.mock.module('nmpi'));
+
+  // Before each test set our injected User factory (_User_) to our local User variable
+  beforeEach(inject(function(_User_) {
+    User = _User_;
+  }));
+
+  // A simple test to verify the nmpi module exists
+  it('should exist User Factory', function() {
+    expect(User).toBeDefined();
+  });
+
+  it('should exist User.get', function() {
+    expect(User.get).toBeDefined();
+  });
+
+});
