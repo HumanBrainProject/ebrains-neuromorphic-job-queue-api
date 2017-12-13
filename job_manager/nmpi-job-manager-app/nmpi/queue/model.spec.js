f99166c95a2b52411eda62027356f38624a1f39a
jojo <jojo@MacBook-Air-de-jojo.local>
2017-12-13 10:37:36 +0100

    test karma jasmine

--- job_manager/nmpi-job-manager-app/nmpi/queue/model.spec.js
+++ job_manager/nmpi-job-manager-app/nmpi/queue/model.spec.js
@@ -12,13 +12,6 @@ describe('Queue factory', function() {
     //window.base_url = '';
     window.ver_api = '/api/v2/';
 
-    // var testComment = {
-    //     id: '1',
-    //     content: 'toto2',
-    //     created_time: '2017-11-14 15:41:49',
-    //     user: 'me',
-    //     job_id: '1',
-    // };
     var testComment = {"content": "ffdfdfsfds", "created_time": "2017-11-30T16:17:21", "id": 1, "job": "/api/v2/results/1", "resource_uri": "/api/v2/comment/1", "user": "me"};
 
     // Before each test load our api.users module
@@ -40,14 +33,6 @@ describe('Queue factory', function() {
         
         // Spy and force the return value when UsersFactory.all() is called
         spyOn(Comment, 'get').and.callThrough();
-        //  spyOn(Comment, 'get').and.callFake(function(arguments, callback) {
-        //     var fakeResult = arguments[0];
-        //     console.log('arguments : ' + JSON.stringify(arguments));
-        //     var callback = arguments[1];
-        //     console.log('callback : ' + callback);
-        //     //return callback(fakeResult);
-        //     return {id: testComment}
-        //  });
     });
 
     it('should exist Queue Factory', function() {
@@ -118,14 +103,7 @@ describe('Queue factory', function() {
         var comment;
 
         // Declare the endpoint we expect our service to hit and provide it with our mocked return values
-        //$httpBackend.whenGET(window.base_url + window.ver_api + "comment/1/").respond(200, $q.when(testComment));
-        //$httpBackend.when('GET', window.base_url + window.ver_api + "comment/1/").respond(testComment);
-        //$httpBackend.whenGET(window.base_url + window.ver_api + "comment/1/").respond(200, $q.when(testComment));
-        //$httpBackend.whenGET(window.base_url + window.ver_api + "comment/1/").respond(testComment);
-        //$httpBackend.whenGET(window.base_url + window.ver_api + "comment/1/").passThrough();
-        //expect(Comment.get).not.toHaveBeenCalled();
         $httpBackend.expectGET(window.base_url + window.ver_api + "comment/1/?format=json").respond(testComment);
-        //$httpBackend.expect('GET', window.base_url + window.ver_api + "comment/1/?format=json").respond(200, $q.when(testComment));
         expect(Comment.get).not.toHaveBeenCalled();
         expect(result).toEqual({});
 
@@ -133,31 +111,34 @@ describe('Queue factory', function() {
 
         rs1 = Comment.get({id:'1'}, function(res){
             //comment = res.success;
-            console.log("toto");
+            //console.log("toto");
             comment = res;
-            console.log('comment 1 : ' + comment);
+            //console.log('comment 1 : ' + comment);
         });
-
-        // rs1 = Comment.get({id:'1'}).then(function(res){
-        //     //comment = res.success;
-        //     console.log("toto");
-        //     comment = res;
-        //     console.log('comment 1 : ' + comment);
-        // });
-
-        //rs1 = Comment.get({id:'1'});        
-        // rs1.then(function(languages){
-        // });
 
         // Flush pending HTTP requests
         $httpBackend.flush();
-        console.log('rs1 : ' + JSON.stringify(rs1));
-        console.log('comment 2 : ' + JSON.stringify(comment));
-        console.log('testComment : ' + JSON.stringify(testComment));
+        // console.log('rs1 : ' + JSON.stringify(rs1));
+        // console.log('comment 2 : ' + JSON.stringify(comment));
+        // console.log('testComment : ' + JSON.stringify(testComment));
         expect(Comment.get).toHaveBeenCalledWith({id:'1'}, jasmine.any(Function));
         expect(comment).toBeDefined();
-        expect(comment.content).toEqual("ffdfdfsfds");
-        //expect(comment).toEqual(testComment);
+        //expect(comment.content).toEqual("ffdfdfsfds");
+        
+        // console.log("testComment.content : " + testComment.content);
+        // console.log("comment.content : " + comment.content);
+        expect(comment.content).toEqual(testComment.content);
+    });
+    
+    it('test result Comment.post', function() {
+        var testPostComment = {"content": "test_post_comment", "id": 3, "job": "/api/v2/results/1", "user": "me"};
+        // post new comment
+        expect(Comment.get).not.toHaveBeenCalled();
+        expect(result).toEqual({});
+
     });
 
+    it('test result Queue.get', function() {
+        var comment;
+    });
   });
