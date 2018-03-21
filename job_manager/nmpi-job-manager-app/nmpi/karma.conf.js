386cebae0c557420c65b5be90ea386ea2196ba5c
jonathanduperrier <jonathan.duperrier@gmail.com>
2018-03-21 17:40:00 +0100

    integration karma with travis

--- job_manager/nmpi-job-manager-app/nmpi/karma.conf.js
+++ job_manager/nmpi-job-manager-app/nmpi/karma.conf.js
@@ -216,7 +216,18 @@ module.exports = function(config) {
   
       // Concurrency level
       // how many browser should be started simultaneous
-      concurrency: Infinity
-    })
+      concurrency: Infinity,
+
+      customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+      },
+
+    });
+    if (process.env.TRAVIS) {
+      configuration.browsers = ['Chrome_travis_ci'];
+    }
   }
   
