dee078c27328170fb02580bb58aa2807b209e9e7
jonathanduperrier <jonathan.duperrier@gmail.com>
2018-05-18 14:20:47 +0200

    test factory RequestListController progress 04

--- resource_manager/coordinator_app/coordinator-app.spec.js
+++ resource_manager/coordinator_app/coordinator-app.spec.js
@@ -52,7 +52,7 @@ describe('Coordinator controller and factory', function() {
 
             spyOn(User, 'get').and.callThrough();
             spyOn(Projects, 'get').and.callThrough();
-            // spyOn(Projects, 'put').and.callThrough();
+            spyOn(Projects, 'update').and.callThrough();
             spyOn(Quotas, 'get').and.callThrough();
             spyOn(Quotas, 'save').and.callThrough();  
 
@@ -62,6 +62,9 @@ describe('Coordinator controller and factory', function() {
         })));
         it('should exist User Factory', function() {
             expect(User).toBeDefined();
+        });
+        it('should exist User.get', function() {
+            expect(User.get).toBeDefined();
         });
         it('test result User.get', function() {
             $httpBackend.expectGET("https://services.humanbrainproject.eu/idm" + window.ver_api + "/user/" + user_test).respond(testUser);
@@ -78,6 +81,14 @@ describe('Coordinator controller and factory', function() {
         it('should exist Projects Factory', function() {
             expect(Projects).toBeDefined();
         });
+        it('should exist Projects.get', function() {
+            expect(Projects.get).toBeDefined();
+        });
+        
+        it('should exist Projects.update', function() {
+            expect(Projects.update).toBeDefined();
+        });
+
         it('test result Projects.get', function(){
             $httpBackend.expectGET(base_url + "/projects/" + project_test).respond(testProject);
             // expect(Projects.get).not.toHaveBeenCalled();
@@ -93,6 +104,28 @@ describe('Coordinator controller and factory', function() {
             expect(result.status).toEqual(testProject.status);
             expect(result.description).toEqual(testProject.description);
         });
+
+        it('test result Projects.update', function(){
+            var update_testProject = {
+                "status": "accepted", 
+                "description": "project01 description updated", 
+                "title": "project01 updated", 
+                "abstract": "project01 abstract updated", 
+                "collab": "4293", 
+                "quotas": [], 
+                "duration": 5, 
+                "context": "1542c7cd-497a-4cf1-a689-c19eac3dc1a0", 
+                "owner": "304621", 
+                "start_date": "2018-05-16", 
+                "resource_uri": "/projects/1542c7cd-497a-4cf1-a689-c19eac3dc1a0"
+            };
+        
+            $httpBackend.expectPUT(base_url + "/projects/" + project_test).respond(200);
+            rs_update = Projects.update(update_testProject);
+            $httpBackend.flush();
+            expect(JSON.stringify(rs_update)).toEqual(JSON.stringify(update_testProject));
+        });
+
         it('should exist Quotas Factory', function() {
             expect(Quotas).toBeDefined();
         });
@@ -133,7 +166,6 @@ describe('Coordinator controller and factory', function() {
                 expect($scope.selectedTab).toEqual(array_tab[i]);
             }
         });
-
     });
 
     describe('RequestDetailController', function(){
@@ -144,7 +176,8 @@ describe('Coordinator controller and factory', function() {
             Projects = _Projects_;
             Quotas = _Quotas_;
             User = _User_;
-            controller = $controller('RequestDetailController', { $scope: $scope });;
+            controller = $controller('RequestDetailController', { $scope: $scope });
+            // $httpBackend.flush();
         })));
         it('RequestDetailController controller should be defined', function() {
             expect(controller).toBeDefined();
@@ -156,6 +189,7 @@ describe('Coordinator controller and factory', function() {
         // });
         if('test addQuota', function($scope){
             $scope.addQuota();
+            $httpBackend.flush();
         });
     });
 });
