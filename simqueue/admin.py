cd8775c38fc9528ec45124d12e4c0b1f44531801
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2019-09-05 15:30:52 +0200

    wip

--- simqueue/admin.py
+++ simqueue/admin.py
@@ -16,3 +16,14 @@ admin.site.register(DataItem)
 admin.site.register(Log)
 
 admin.site.register(Comment)
+
+
+
+from django.contrib.sessions.models import Session
+
+class SessionAdmin(admin.ModelAdmin):
+
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ['session_key', '_session_data', 'expire_date']
+admin.site.register(Session, SessionAdmin)
