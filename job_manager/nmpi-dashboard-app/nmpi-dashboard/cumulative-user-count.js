da0df028a953d0f7d4ce9a6fdf2231ecf9b21622
Andrew Davison <andrew.davison@cnrs.fr>
2021-10-05 14:35:15 +0200

    Missed adding a couple of BrainScaleS-2 graphs to dashboard

--- job_manager/nmpi-dashboard-app/nmpi-dashboard/cumulative-user-count.js
+++ job_manager/nmpi-dashboard-app/nmpi-dashboard/cumulative-user-count.js
@@ -108,6 +108,59 @@ d3.json("/api/v2/statistics/cumulative-user-count/?platform=BrainScaleS",
     }
 );
 
+d3.json("/api/v2/statistics/cumulative-user-count/?platform=BrainScaleS-2",
+    function(error, data) {
+        if (error) return console.warn(error);
+        console.info(data);
+        var dates = data.dates;
+        var values = data.values;
+
+        values.unshift('users');
+        dates.unshift('x');
+
+
+        var chart = c3.generate({
+            bindto: '#chart-cumulative-user-count-BrainScaleS-2',
+            data: {
+                x: 'x',
+                xFormat: '%Y-%m-%d',
+                columns: [dates, values],
+                type: 'area-step'
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d'
+                    }
+                },
+                y: {
+                    label: {
+                        position: 'outer-middle',
+                        text: 'Number of BrainScaleS-2 users'
+                    },
+                    min: 0,
+                    padding: {top: 0, bottom: 0}
+                }
+            },
+            line: {
+                step: {
+                    type: 'step-after'
+                }
+            },
+            legend: {
+                show: false
+            },
+            padding: {
+                top: 20,
+                right: 80,
+                bottom: 20,
+                left: 80,
+            },
+        });
+    }
+);
+
 d3.json("/api/v2/statistics/cumulative-user-count/?platform=SpiNNaker",
     function(error, data) {
         if (error) return console.warn(error);
