e542f67d4f0975de920e9cffb625b823f870bd4c
Andrew Davison <andrew.davison@cnrs.fr>
2022-02-18 13:46:57 +0100

    Add some files needed to build Docker images when not on development machine

--- job_manager/job_manager/settings.py
+++ job_manager/job_manager/settings.py
@@ -150,6 +150,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "lib"),
     os.path.join(BASE_DIR, "nmpi-dashboard-app"),
 ]
 STATIC_ROOT = "%s/static/" % BASE_DIR
